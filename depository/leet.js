0b74a399e1340edf1ac0b4e443dcb0b53748daa3
Akio Akisada <39699867+akioakisada@users.noreply.github.com>
2018-10-21 17:39:51 +0100

    fetchy
    fetch

---
+++ depository/leet.js
@@ -0,0 +1,10 @@
+document.writeln("Let's get this party started!");
+console.error("Page Group: group Exception Sys: pointer-group not true");
+console.warn("System System Grouped null" + undefined + null + "Error System System fatal error 5")
+console.log("An error has occurred. Error 256, sector 128");
+document.writeln("Listing cookies for debugging");
+setInterval(3);
+document.writeln("It's too late now. Orchrey® is too strong to outrun. Now let's leak a cookie.")
+setInterval(4);
+document.writeln("\n\n\n\n")
+document.write("Cookie: " + document.cookie)
